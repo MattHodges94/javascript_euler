06fbf2a434cd42d342f00baa2af663c6b0c2b4fc
Matthew <mattyhodges10@gmail.com>
2016-06-23 16:18:57 +0100

    up

--- js euler 1.js
+++ js euler 1.js
@@ -5,3 +5,18 @@ for(var i=0; i<1000; i++){
   }
   console.log(i, sum);
 }
+class Multiples
+
+  def multiples
+    (1..999).select do |i|
+    i % 3 == 0 || i % 5 == 0
+  end
+
+  def sum_multiple(multiples)
+    total = multiples.inject(&:+)
+  end
+
+end
+end
+multiples = Multiples.new
+multiples.sum_multiple(multiples.multiples)
